bf23d1b50757eccdae016b06f340d2d7a238677b
Yozi Heru Maulana <zirocraftid@gmail.com>
2026-03-14 10:17:31 +0700

    feat: added Toaster provider to App root for global notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+// 1. Import Toaster-nya
+import { Toaster } from "react-hot-toast";
 import Menubar from "./components/Menubar/Menubar.jsx";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 import ManageCategory from "./pages/ManageCategory/ManageCategory.jsx";
@@ -10,6 +12,9 @@ import Explore from "./pages/Explore/Explore.jsx";
 const App = () => {
   return (
     <div>
+      {/* 2. Taruh Toaster di sini agar posisinya di atas semua elemen */}
+      <Toaster position="top-center" reverseOrder={false} />
+
       <Menubar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
